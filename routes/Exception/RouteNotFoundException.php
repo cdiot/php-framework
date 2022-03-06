ddd36cd7ec8a7844cc9ac828918a670741cdce39
Chris <christopher.diot5@gmail.com>
2022-03-06 16:45:48 +0100

    Add routing system + test

---
+++ routes/Exception/RouteNotFoundException.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of the Routing system.
+ * 
+ * PHP version 8.1
+ * 
+ * @category Routing
+ * @package  Routing/Exception
+ * @link     https://github.com/cdiot/php-framework
+ */
+
+namespace Routing\Exception;
+
+use Exception;
+use Throwable;
+
+/**
+ * The resource was not found.
+ *
+ * This exception should trigger an HTTP 404 response in your application code.
+ * 
+ * @category Routing
+ * @package  src/Routing/Exception
+ * @link     https://github.com/cdiot/php-framework
+ */
+class RouteNotFoundException extends Exception
+{
+    /**
+     * Constructor class
+     * 
+     * @param string         $message  message
+     * @param int            $code     code
+     * @param Throwable|null $previous previous
+     */
+    public function __construct($message = "", $code = 404, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Force to string
+     * 
+     * @return [type]
+     */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+    /**
+     * Show error
+     * 
+     * @return [type]
+     */
+    public function error404()
+    {
+        http_response_code(404);
+    }
+}
---
+++ tests/RouterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Tests.
+ * 
+ * PHP version 8.1
+ * 
+ * @category Tests
+ * @package  tests
+ * @link     https://github.com/cdiot/php-framework
+ */
+
+namespace App\Tests;
+
+use Routing\Exception\RouteNotFoundException;
+use Routing\Route;
+use Routing\Router;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class RouterTest.
+ * 
+ * @category Tests
+ * @package  tests
+ * @link     https://github.com/cdiot/php-framework
+ */
+class RouterTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
