c7510715020dd257912b6dc0822da7fb52d29418
Chris <christopher.diot5@gmail.com>
2022-03-07 12:55:29 +0100

    Add authenticate middleware

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part Middleware.
+ * 
+ * PHP version 8.1
+ * 
+ * @category Middleware
+ * @package  app/Http/Middleware
+ * @link     https://github.com/cdiot/php-framework
+ */
+
+namespace App\Http\Middleware;
+
+use Routing\Exception\RouteNotFoundException;
+
+/**
+ * Authenticate
+ * 
+ * @category Middleware
+ * @package  app/Http/Middleware
+ * @link     https://github.com/cdiot/php-framework
+ */
+class Authenticate
+{
+    /**
+     * Authenticate constructor.
+     */
+    public function __construct()
+    {
+        if (!$this->_authorize()) {
+            throw new RouteNotFoundException("Access forbidden.");
+        }
+    }
+
+    /**
+     * Allow access to auth
+     * 
+     * @return [type]
+     */
+    private function _authorize()
+    {
+        return $_SESSION['auth'] != null;
+    }
+}
