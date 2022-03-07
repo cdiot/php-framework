dabcc19c11b47fa566c821ecea69ed5d999d1a43
Chris <christopher.diot5@gmail.com>
2022-03-07 13:41:45 +0100

    Add register controller + view

--- public/index.php
+++ public/index.php
@@ -16,6 +16,8 @@ try {
     $router->get(new Route('/', ['controller' => 'App\Http\Controllers\HomeController@index']));
     $router->get(new Route('/login', ['controller' => 'App\Http\Controllers\LoginController@displayAuthenticateForm']));
     $router->post(new Route('/login', ['controller' => 'App\Http\Controllers\LoginController@authenticate']));
+    $router->get(new Route('/register', ['controller' => 'App\Http\Controllers\RegisterController@displayRegisterForm']));
+    $router->post(new Route('/register', ['controller' => 'App\Http\Controllers\RegisterController@register']));
     $router->getRoutes();
 } catch (RouteNotFoundException $e) {
     return $e->error404();
