f01c66f83bdf86ccf1dae948fa3fd4fa193404db
Chris <christopher.diot5@gmail.com>
2022-03-09 12:14:25 +0100

    Add web.php in routes to register web routes

--- public/index.php
+++ public/index.php
@@ -12,15 +12,6 @@ $dotenv->load();
 $router = new Router($_GET['url']);
 
 try {
-    Router::get('/', ['controller' => 'App\Http\Controllers\HomeController@index']);
-    Router::get('/login', ['controller' => 'App\Http\Controllers\LoginController@displayAuthenticateForm']);
-    Router::post('/login', ['controller' => 'App\Http\Controllers\LoginController@authenticate']);
-    Router::get('/register', ['controller' => 'App\Http\Controllers\RegisterController@displayRegisterForm']);
-    Router::post('/register', ['controller' => 'App\Http\Controllers\RegisterController@register']);
-    Router::get('/tasks', ['controller' => 'App\Http\Controllers\TaskController@index', 'middleware' => 'Authenticate']);
-    Router::get('/task/:id', ['controller' => 'App\Http\Controllers\TaskController@show', 'middleware' => 'Authenticate'], ['id', '[0-9]+']);
-    Router::get('/add/task', ['controller' => 'App\Http\Controllers\TaskController@create', 'middleware' => 'Authenticate']);
-    Router::post('/add/task', ['controller' => 'App\Http\Controllers\TaskController@store', 'middleware' => 'Authenticate']);
     Router::getRoutes();
 } catch (RouteNotFoundException $e) {
     return $e->error404();
